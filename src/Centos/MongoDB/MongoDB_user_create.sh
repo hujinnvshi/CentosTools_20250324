fbc87d3c3eaa17636b640a3374e73c187ace741f
瑞德 <redelegocn@gmail.com>
2025-07-26 08:39:53 +0800

    docs(Centos/MongoDB): 更新MongoDB用户创建脚本的注释
    添加远程连接MongoDB的示例命令，方便参考使用

--- src/Centos/MongoDB/MongoDB_user_create.sh
+++ src/Centos/MongoDB/MongoDB_user_create.sh
@@ -13,3 +13,7 @@ db.createUser({
 mongosh "mongodb://admin1:admin1@172.16.47.185:27017/admin"
 use admin
 db.getUsers()
+
+
+
+# mongosh --host 172.16.47.185 --port 27017 -u admin -p 'Secsmart#612' --authenticationDatabase admin --eval "db.runCommand({connectionStatus: 1})"
