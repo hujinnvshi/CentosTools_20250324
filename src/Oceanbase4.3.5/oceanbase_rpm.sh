106b253fb2e5655efe8ea59e4560602cd4140dae
瑞德 <redelegocn@gmail.com>
2025-08-04 10:28:03 +0800

    feat: 添加每分钟时间同步和OceanBase高性能配置
    - 在TiaoU.ai中添加每分钟执行chronyc强制时间同步的cron任务
    - 在oceanbase_rpm.sh中添加obd pref高性能配置和多个连接示例
    - 更新OceanBase连接信息包括IP地址和端口配置

--- src/Oceanbase4.3.5/oceanbase_rpm.sh
+++ src/Oceanbase4.3.5/oceanbase_rpm.sh
@@ -1,8 +1,27 @@
 bash -c "$(curl -s https://obbusiness-private.oss-cn-shanghai.aliyuncs.com/download-center/opensource/oceanbase-all-in-one/installer.sh)"
 source ~/.oceanbase-all-in-one/bin/env.sh
+
 obd demo
+# 高性能
+obd pref
 
 CREATE USER 'admin'@'%' IDENTIFIED BY 'Secsmart#612';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
 
-obclient -h172.16.47.185 -uadmin -P 2881 -pSecsmart#612
+obclient -h192.168.0.105 -uadmin -P 2881 -pSecsmart#612 -D oceanbase
+
+obclient -h192.168.0.106 -P2883 -uroot@sys -p'9G.CK8Tk' -Doceanbase -A 
+
+
++-------------------------------------------------+
+|                   oceanbase-ce                  |
++---------------+---------+------+-------+--------+
+| ip            | version | port | zone  | status |
++---------------+---------+------+-------+--------+
+| 192.168.0.105 | 4.2.1.8 | 2881 | zone1 | ACTIVE |
+| 192.168.0.106 | 4.2.1.8 | 2881 | zone2 | ACTIVE |
++---------------+---------+------+-------+--------+
+
+obclient -h192.168.0.105 -P2881 -uroot@sys -p'9G.CK8Tk' -Doceanbase -A
+
+obclient -h192.168.0.106 -P2881 -uroot@sys -p'9G.CK8Tk' -Doceanbase -A
