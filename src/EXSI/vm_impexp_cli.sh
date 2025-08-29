30d51598613627a39b84ccfc1ba55a05d1535ddb
zhangning <zhangning@163.com>
2025-08-29 11:13:12 +0800

    exsi 命令行操作指令。

--- src/EXSI/vm_impexp_cli.sh
+++ src/EXSI/vm_impexp_cli.sh
@@ -1,8 +1,8 @@
 
-# windows 导出exsi vm
-D:\ovftool\ovftool.exe --noSSLVerify "vi://root:Secsmart#612@192.168.20.36/app/oracle10g" "D:\oracle10g.ova"
+# windows 导出exsi vm(远程,OK)
+D:\ovftool\ovftool.exe --noSSLVerify "vi://root:Secsmart#612@172.16.48.15/c76-172.16.48.58-kdcv2" "D:\c76-172.16.48.58-kdcv2.ovf"
 
-# windows 导出workstation vm
+# windows 导出workstation vm(本地,OK)
 D:\ovftool\ovftool.exe "D:\C48O9I\C48O9I.vmx" "D:\c48o9i_exp"
 
 # PowerShell 手动打包
